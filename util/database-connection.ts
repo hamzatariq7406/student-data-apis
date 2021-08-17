5f7586d53add096c50faef7dad11882a82eb7b1e
Hamza Tariq <tariqh1@netsolpk.com>
2021-08-16 17:33:55 -0700

    xxxyyzzz

--- util/database-connection.ts
+++ util/database-connection.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 
+console.log(process.env.DATABASE_PASSWORD);
 //connecting to MySql by providing required configuration
 const sequelize: any = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_FOLDER, process.env.DATABASE_PASSWORD, {
   dialect: "mysql",
